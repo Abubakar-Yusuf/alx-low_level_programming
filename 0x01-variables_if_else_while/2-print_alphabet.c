316eac80ae1ac3e6df150c351d494d31e1f6a5d2
Abubakar Sidiq <yusufabubakarsidiq@gmail.com>
2022-10-15 10:01:06 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-/*
- * main - Entry
+/**
+ * main - print alpha in lower case
  * Return: Always 0
  */
 
-int main (void)
+int main(void)
 {
 	char low;
 
