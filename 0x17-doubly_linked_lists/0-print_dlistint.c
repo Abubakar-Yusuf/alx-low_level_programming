8f02a5cc22e37c6e3ed62bf03aa9ea84cdc8032f
Abubakar Yusuf <yusufabubakarsidiq@gmail.com>
2023-01-13 13:51:41 -0800

    task 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,26 +1,27 @@
-/*
- * File: 0-print_dlistint.c
- * Auth: Gedeon Obae Gekonge
- */
-
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a dlistint_t list.
- * @h: The head of the dlistint_t list.
- * Return: The number of nodes in the list.
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: pointer to the head of the list
+ * Return: number of nodes
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes = 0;
+	int counter = 0;
 	
-	while (h)
+	if (h == NULL)
+		return (counter);
+	
+	while (h->prev != NULL)
+		h = h->prev;
+	
+	while (h != NULL)
 	{
-		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
+		counter++;
 	}
-
-	return (nodes);
+	
+	return (counter);
 }
